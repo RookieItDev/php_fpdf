2c7a9297c7a71fd9a69016ca9131000fd1c3c8cd
RookieItDev <peerapong.it-st@rmutsb.ac.th>
2022-03-24 20:22:51 +0700

    update barcode

--- test_barcode2.php
+++ test_barcode2.php
@@ -2,7 +2,7 @@
 
 require('code128.php');
 //require('fpdf.php'); // แทรกไฟล์ที่เก็บฟังก์ชันของไฟล์  PDF
-$pdf=new PDF_Code128('P','mm',array(150,150));
+$pdf=new PDF_Code128('P','mm',array(100,100));
 $pdf->AddPage();
 $pdf->AddFont('sarabun','B','THSarabunB.php'); // แทกฟอนต์  (sarabun เรากำหนดเป็นชื่ออะไรก็ได้)  ฟอนต์ตัวหนา
 $pdf->AddFont('sarabun','','THSarabun.php'); // ฟอนต์ตัวปกติ
@@ -11,12 +11,12 @@ $pdf->SetFont('sarabun','',14);
         //this is the first method
        
         $code='123456789012';
-        $pdf->Code128(30,3,$code,80,15,'C');//ตำแหน่ง 1 = ห่างจากซ้าย , ตำแหน่ง 2 = ห่างจากบน , ตำแหน่ง 3 = ความกว้าง barcode , ตำแหน่ง 4 = ขนาด Font 
+        $pdf->Code128(10,3,$code,80,15,'C');//ตำแหน่ง 1 = ห่างจากซ้าย , ตำแหน่ง 2 = ห่างจากบน , ตำแหน่ง 3 = ความกว้าง barcode , ตำแหน่ง 4 = ขนาด Font 
         //$pdf->Cell(5,22,iconv('utf-8','cp874','ลำดับ'),0,100,'C');
-        $pdf->Cell(1,22,iconv('utf-8','cp874','ชื่อสินค้า : '.$code.'    '.
-        "                            ".
+        $pdf->Cell(1,22,iconv('utf-8','cp874','ชื่อสินค้า : '.$code.''.
+        "    ".
         ' รหัสสินค้า : '.$code.''),0,20);
-        $pdf->Cell(1,-10,iconv('utf-8','cp874','ลำดับ'),0,0);
+        $pdf->Cell(1,-10,iconv('utf-8','cp874','RowNumber:'.$i.''),0,0);
         // $pdf->SetXY(5,20,150,15);//ความห่างของตัวอักษร
         // $pdf->Write(5,'ตัวอย่าง1: "'.$code.' "    " B Set: "'.$code.'"'." ");
         // $pdf->Write(5,'A set: "'.$code.' "    " B Set: "'.$code.'"'." ");
